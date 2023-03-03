ad4d4f0c346687a4b4cba4cfcc2cc46da35447c7
Aleksandra Zhi <jiterewa@gmail.com>
2023-03-03 17:00:46 +0000

    Updated navigation bar to the new style

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,41 @@
+const targetShow = document.querySelectorAll('#section__services')
+const targetHide = document.querySelector('#section__hero')
+const optionsShow = {
+	threshold: 0.3,
+}
+const optionsHide = {
+	threshold: 1,
+}
 
+function handleIntersectionShow(entries) {
+	entries.map(entry => {
+		if (entry.isIntersecting) {
+			console.log('show')
+			document
+				.querySelector('#header__nav--main .navbar')
+				.classList.add('active')
+			observerShow.unobserve(entry.target)
+		}
+	})
+}
+
+function handleIntersectionHide(entry) {
+	if (entry.isIntersecting) {
+		console.log('hide')
+		document
+			.querySelector('#header__nav--main .navbar')
+			.classList.remove('active')
+		observerHide.unobserve(entry.target)
+	}
+}
+
+const observerShow = new IntersectionObserver(
+	handleIntersectionShow,
+	optionsShow
+)
+const observerHide = new IntersectionObserver(
+	handleIntersectionHide,
+	optionsHide
+)
+targetShow.forEach(target => observerShow.observe(target))
+observerHide.observe(targetHide)
