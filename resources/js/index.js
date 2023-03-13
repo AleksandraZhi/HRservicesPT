22bc73bc1d663c5a4356b139257a358eae9de726
Aleksandra Zhi <jiterewa@gmail.com>
2023-03-13 21:58:25 +0000

    Updated vacancies section with submit button

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,26 +1,41 @@
 const targetShow = document.querySelectorAll('section:not(#section__hero)')
 const targetHide = document.querySelector('#observer-hero')
+let currentUrl = window.location.href
+let section = ''
+let page = ''
 
 // below fixing issue with navbar links not scrolling to the right section because of the fixed navbar
 document.addEventListener('click', function (event) {
 	if (!event.target.matches('#header__nav--main .nav-link')) return
 	event.preventDefault()
-	const section = document.querySelector(event.target.dataset.href)
-	const page = document.querySelector(event.target.dataset.page)
-	let currentUrl = window.location.href
-	if (!currentUrl.includes('index.html')) {
-		currentUrl =
-			currentUrl
-				.split('/')
-				.filter(each => !each.includes('vacancies.html'))
-				.join('/') + '/index.html'
-		window.location.href = currentUrl
-	}
-	document.addEventListener('DOMContentLoaded', function (event) {
+	section = document.querySelector(event.target.dataset.href)
+	page = document.querySelector(event.target.dataset.page)
+	// if (!currentUrl.includes('index.html')) {
+	// 	currentUrl =
+	// 		currentUrl
+	// 			.split('/')
+	// 			.filter(each => !each.includes('vacancies.html'))
+	// 			.join('/') + '/index.html'
+	// 	console.log(section, page)
+	// 	window.location.href = currentUrl
+	// 	setTimeout(function () {
+	// 		const scrollDistance =
+	// 			window.pageYOffset + section.getBoundingClientRect().top - 85
+	// 		window.scroll(0, scrollDistance)
+	// 		console.log(section, page)
+	// 	}, 5000)
+	// } else {
+	// 	setTimeout(function () {
+	// 		const scrollDistance =
+	// 			window.pageYOffset + section.getBoundingClientRect().top - 85
+	// 		window.scroll(0, scrollDistance)
+	// 	}, 2000)
+	// }
+	if (currentUrl.includes('index.html')) {
 		const scrollDistance =
 			window.pageYOffset + section.getBoundingClientRect().top - 85
 		window.scroll(0, scrollDistance)
-	})
+	}
 })
 
 // below is observer to highlight navbar when scrolling down
