a151993129ca93df6b999016d85aa2f0b92cc114
Aleksandra Zhi <jiterewa@gmail.com>
2023-03-19 17:54:38 +0000

    Updated vacancies page to section

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,41 +1,15 @@
 const targetShow = document.querySelectorAll('section:not(#section__hero)')
 const targetHide = document.querySelector('#observer-hero')
-let currentUrl = window.location.href
 let section = ''
-let page = ''
 
 // below fixing issue with navbar links not scrolling to the right section because of the fixed navbar
 document.addEventListener('click', function (event) {
 	if (!event.target.matches('#header__nav--main .nav-link')) return
 	event.preventDefault()
 	section = document.querySelector(event.target.dataset.href)
-	page = document.querySelector(event.target.dataset.page)
-	// if (!currentUrl.includes('index.html')) {
-	// 	currentUrl =
-	// 		currentUrl
-	// 			.split('/')
-	// 			.filter(each => !each.includes('vacancies.html'))
-	// 			.join('/') + '/index.html'
-	// 	console.log(section, page)
-	// 	window.location.href = currentUrl
-	// 	setTimeout(function () {
-	// 		const scrollDistance =
-	// 			window.pageYOffset + section.getBoundingClientRect().top - 85
-	// 		window.scroll(0, scrollDistance)
-	// 		console.log(section, page)
-	// 	}, 5000)
-	// } else {
-	// 	setTimeout(function () {
-	// 		const scrollDistance =
-	// 			window.pageYOffset + section.getBoundingClientRect().top - 85
-	// 		window.scroll(0, scrollDistance)
-	// 	}, 2000)
-	// }
-	if (currentUrl.includes('index.html')) {
-		const scrollDistance =
-			window.pageYOffset + section.getBoundingClientRect().top - 85
-		window.scroll(0, scrollDistance)
-	}
+	const scrollDistance =
+		window.pageYOffset + section.getBoundingClientRect().top - 85
+	window.scroll(0, scrollDistance)
 })
 
 // below is observer to highlight navbar when scrolling down
